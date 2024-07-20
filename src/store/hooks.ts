047fe7a4d2c79ea77f34edf772ce4ed99977b393
Michal <michalgrzegorczyk04@gmail.com>
2024-07-20 11:40:32 +0200

    create a type-safe useSelector hook

--- src/store/hooks.ts
+++ src/store/hooks.ts
@@ -1,8 +1,13 @@
 /** @format */
 
-import { useDispatch } from "react-redux";
-import { AppDispatch } from "./store";
+import {
+  type TypedUseSelectorHook,
+  useDispatch,
+  useSelector,
+} from "react-redux";
+import { AppDispatch, RootState } from "./store";
 
 type DispatchFunction = () => AppDispatch;
 
 export const useCartDispatch: DispatchFunction = useDispatch;
+export const useCartSelector: TypedUseSelectorHook<RootState> = useSelector;
