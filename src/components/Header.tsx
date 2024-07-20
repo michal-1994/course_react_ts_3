08c436474fca156470ba2f57f979e225672898f3
Michal <michalgrzegorczyk04@gmail.com>
2024-07-20 11:48:39 +0200

    select and transform redux store data

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,15 @@
-import { useState } from 'react';
+/** @format */
 
-import Cart from './Cart.tsx';
+import { useState } from "react";
+
+import Cart from "./Cart.tsx";
+import { useCartSelector } from "../store/hooks.ts";
 
 export default function Header() {
   const [cartIsVisible, setCartIsVisible] = useState(false);
+  const cartQuantity = useCartSelector((state) =>
+    state.cart.items.reduce((val, item) => val + item.quantity, 0)
+  );
 
   function handleOpenCartClick() {
     setCartIsVisible(true);
@@ -16,13 +22,13 @@ export default function Header() {
   return (
     <>
       {cartIsVisible && <Cart onClose={handleCloseCartClick} />}
-      <header id="main-header">
-        <div id="main-title">
-          <img src="logo.png" alt="Elegant model" />
+      <header id='main-header'>
+        <div id='main-title'>
+          <img src='logo.png' alt='Elegant model' />
           <h1>Elegant Redux</h1>
         </div>
         <p>
-          <button onClick={handleOpenCartClick}>Cart (0)</button>
+          <button onClick={handleOpenCartClick}>Cart ({cartQuantity})</button>
         </p>
       </header>
     </>
