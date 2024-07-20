d59e4612bf040e245650e381cc3e3f1d090c751a
Michal <michalgrzegorczyk04@gmail.com>
2024-07-20 11:17:23 +0200

    add logic to reducer

--- src/store/cart-slice.ts
+++ src/store/cart-slice.ts
@@ -24,7 +24,30 @@ export const cartSlice = createSlice({
     addToCart: (
       state,
       action: PayloadAction<{ id: string; title: string; price: number }>
-    ) => {},
-    removeFromCart: () => {},
+    ) => {
+      const itemIndex = state.items.findIndex(
+        (item) => item.id === action.payload.id
+      );
+
+      if (itemIndex >= 0) {
+        state.items[itemIndex].quantity++;
+      } else {
+        state.items.push({
+          ...action.payload,
+          quantity: 1,
+        });
+      }
+    },
+    removeFromCart: (state, action: PayloadAction<string>) => {
+      const itemIndex = state.items.findIndex(
+        (item) => item.id === action.payload
+      );
+
+      if (state.items[itemIndex].quantity === 1) {
+        state.items.splice(itemIndex, 1);
+      } else {
+        state.items[itemIndex].quantity--;
+      }
+    },
   },
 });
